23d18e7e8e1b512aec7e06f6ade96aa87b02ffaf
Tim Hui <thui@gatech.edu>
2012-05-02 11:36:11 -0400

    added image caching, removed images

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -1,5 +1,6 @@
 require 'RMagick'
 require 'rubygems'
+include Magick
 
 
 class IconsController < ApplicationController
@@ -9,7 +10,7 @@ class IconsController < ApplicationController
   # GET /icons
   # GET /icons.json
   def index
-    @icons = Icon.all
+    @icons = Icon.find(:all, :limit=>20)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -31,14 +32,7 @@ class IconsController < ApplicationController
   # GET /icons/new
   # GET /icons/new.json
   def new
-    generate_image('blue',0.75)
 
-    @icon = Icon.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @icon }
-    end
   end
 
   # GET /icons/1/edit
@@ -91,21 +85,27 @@ class IconsController < ApplicationController
   end
 
    def generate_image(color,opacity)
-    canvas = Magick::ImageList.new
-    canvas.new_image(250,250,Magick::HatchFill.new('white','gray90'))
-    circle = Magick::Draw.new
-    circle.stroke(color)
-    circle.fill_opacity(0)
-    circle.stroke_opacity(opacity)
-    circle.stroke_width(3)
-    circle.stroke_linecap('round')
-    circle.stroke_linejoin('round')
-    circle.ellipse(canvas.rows/2,canvas.columns/2,80,80,0,315)
-    circle.polyline(180,70, 173,78, 190,78, 191,62)
-    circle.draw(canvas)
-    canvas.write(color+".gif")
-    canvas.display
-    exit
+    file_path = "public/images/icons/"+color+".gif"
+    if FileTest.exists?(file_path)
+      render :text => file_path
+      return file_path
+    else
+      canvas = Magick::ImageList.new
+      canvas.new_image(250,250,Magick::HatchFill.new('white','gray90'))
+      circle = Magick::Draw.new
+      circle.stroke(color)
+      circle.fill_opacity(0)
+      circle.stroke_opacity(opacity)
+      circle.stroke_width(3)
+      circle.stroke_linecap('round')
+      circle.stroke_linejoin('round')
+      circle.ellipse(canvas.rows/2,canvas.columns/2,80,80,0,315)
+      circle.polyline(180,70, 173,78, 190,78, 191,62)
+      circle.draw(canvas)
+      canvas.write file_path
+      canvas.display
+      return file_path
+    end
  end
 
 end
