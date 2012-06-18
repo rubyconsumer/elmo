791db2ab5499590efa2a5c6ce57c836438fc8d6f
Tim Hui <thui@gatech.edu>
2012-06-18 11:54:45 -0400

    moved icon generating function to model

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -3,20 +3,12 @@ class IconsController < ApplicationController
   caches_page :index, :new
 
   def show
-      image_url = generate_image("#" + params[:color])
+      image_url = Icon.generate("#" + params[:color])
       response.headers['Content-Type'] = 'image/gif'
       response.headers['Content-Disposition'] = 'inline'
       render :text => open(image_url).read        
       
   end
-  def generate_image(color)
-    file_path = "tmp/icons/"+color+".png"
-    if !FileTest.exists?(file_path)
-      marker = Magick::Image.read("public/images/icons/marker.png")
-      colored = marker[0].color_floodfill(5, 5, color)     
-      colored.write(file_path)
-    end                     
-    return file_path
- end
+
 
 end
