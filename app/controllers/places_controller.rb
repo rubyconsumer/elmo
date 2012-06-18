f81b2cc3ae48de894a73239407361b4ed31afd1f
Tim Hui <thui@gatech.edu>
2012-06-18 11:38:58 -0400

    removed space from places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with ELMO.  If not, see <http://www.gnu.org/licenses/>.
 # 
-
 class PlacesController < ApplicationController
   def index
     @places = load_objects_with_subindex(Place)
