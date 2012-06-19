38c9260aebb7aae9e449191ec0e12ca6e51ae78d
Tim Hui <thui@gatech.edu>
2012-06-19 10:42:05 -0400

    modified icon.rb

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -1,2 +1,13 @@
-class Icon < ActiveRecord::Base
+class Icon < ActiveRecord::Base        
+  
+   def self.generate(color)
+     file_path = "tmp/icons/"+color+".png"
+     if !FileTest.exists?(file_path)
+       marker = Magick::Image.read("public/images/icons/marker.png")
+       colored = marker[0].color_floodfill(5, 5, color)     
+       colored.write(file_path)
+     end                     
+     return file_path
+  end  
+  
 end
